fccc9e804b37ae804fbdabd529969fbb014e4bc0
Diptanu Roy <49843997+infernal9999@users.noreply.github.com>
2023-07-04 16:12:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ fetch('data.json')
   .then(response => response.json())
   .then(data => {
     const familyTree = document.getElementById('familyTree');
+    const popup = document.getElementById('popup');
 
     // Create a person container with avatar and name
     function createPersonContainer(person) {
@@ -20,7 +21,58 @@ fetch('data.json')
       name.textContent = person.NAME;
       container.appendChild(name);
 
+      container.addEventListener('click', () => {
+        showPopup(person);
+      });
+
       return container;
+    }
+
+    // Show popup window with person details
+    function showPopup(person) {
+      popup.innerHTML = ''; // Clear previous content
+
+      const avatar = document.createElement('img');
+      avatar.className = 'popup-avatar';
+      avatar.src = person.SEX === 'MALE' ? 'male_avatar.png' : 'female_avatar.png';
+      avatar.alt = person.NAME;
+      popup.appendChild(avatar);
+
+      const details = document.createElement('div');
+      details.className = 'popup-details';
+
+      for (const [key, value] of Object.entries(person)) {
+        if (key !== 'SEX' && key !== 'CHILDREN' && value !== 'NA') {
+          const row = document.createElement('div');
+          row.className = 'popup-row';
+
+          const label = document.createElement('div');
+          label.className = 'popup-label';
+          label.textContent = key;
+          row.appendChild(label);
+
+          const text = document.createElement('div');
+          text.className = 'popup-text';
+          text.textContent = value;
+          row.appendChild(text);
+
+          details.appendChild(row);
+        }
+      }
+
+      popup.appendChild(details);
+      popup.style.display = 'block';
+
+      // Close popup when clicking outside the container or on another container
+      document.addEventListener('click', closePopup);
+    }
+
+    // Close popup window
+    function closePopup(event) {
+      if (!event.target.closest('.person-container')) {
+        popup.style.display = 'none';
+        document.removeEventListener('click', closePopup);
+      }
     }
 
     // Recursive function to build the family tree
