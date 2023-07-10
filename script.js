8eeb9a427abf33ed47b7b78a5d46640523e639ab
Diptanu Roy <49843997+infernal9999@users.noreply.github.com>
2023-07-10 17:40:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,17 +23,52 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const createFamilyTree = (members) => {
     const treeWrapper = document.createElement("div");
+    treeWrapper.className = "family-tree";
+
+    const levels = [[]];
+    let currentLevel = 0;
 
     members.forEach((member) => {
       const memberContainer = createMemberContainer(member);
+      levels[currentLevel].push(memberContainer);
 
-      if (member.children && member.children.length > 0) {
-        const childrenWrapper = createFamilyTree(member.children);
-        memberContainer.appendChild(childrenWrapper);
+      if (member.WIFE || member.SIBLINGS) {
+        const spouseAndSiblingsContainer = document.createElement("div");
+        spouseAndSiblingsContainer.className = "level-0";
+        spouseAndSiblingsContainer.appendChild(memberContainer);
+
+        if (member.WIFE) {
+          const spouseContainer = createSpouseContainer(member.WIFE);
+          spouseAndSiblingsContainer.appendChild(spouseContainer);
+        }
+
+        if (member.SIBLINGS && member.SIBLINGS.length > 0) {
+          const siblingsContainer = createSiblingsContainer(member.SIBLINGS);
+          spouseAndSiblingsContainer.appendChild(siblingsContainer);
+        }
+
+        treeWrapper.appendChild(spouseAndSiblingsContainer);
+      } else {
+        treeWrapper.appendChild(memberContainer);
       }
 
-      treeWrapper.appendChild(memberContainer);
+      if (member.CHILDREN && member.CHILDREN.length > 0) {
+        const childrenContainer = createChildrenContainer(member.CHILDREN);
+        levels[currentLevel + 1] = levels[currentLevel + 1] || [];
+        levels[currentLevel + 1].push(childrenContainer);
+      }
     });
+
+    for (let i = 0; i < levels.length; i++) {
+      const levelContainer = document.createElement("div");
+      levelContainer.className = `level-${i}`;
+
+      levels[i].forEach((container) => {
+        levelContainer.appendChild(container);
+      });
+
+      treeWrapper.appendChild(levelContainer);
+    }
 
     return treeWrapper;
   };
@@ -97,6 +132,89 @@ document.addEventListener("DOMContentLoaded", function() {
 
     container.appendChild(image);
     container.appendChild(name);
+
+    return container;
+  };
+
+  const createSpouseContainer = (spouse) => {
+    const container = document.createElement("div");
+    container.className = "container spouse";
+
+    const imageSrc = `images/${spouse.replace(/\s+/g, "").toLowerCase()}.png`;
+    const fallbackImageSrc = "images/female_avatar.png";
+
+    const image = document.createElement("img");
+    image.src = imageSrc;
+    image.onerror = () => {
+      image.src = fallbackImageSrc;
+    };
+
+    const name = document.createElement("div");
+    name.className = "name";
+    name.textContent = spouse;
+
+    container.appendChild(image);
+    container.appendChild(name);
+
+    return container;
+  };
+
+  const createSiblingsContainer = (siblings) => {
+    const container = document.createElement("div");
+    container.className = "container siblings";
+
+    siblings.forEach((sibling) => {
+      const siblingContainer = document.createElement("div");
+      siblingContainer.className = "container sibling";
+
+      const imageSrc = `images/${sibling.replace(/\s+/g, "").toLowerCase()}.png`;
+      const fallbackImageSrc = "images/male_avatar.png";
+
+      const image = document.createElement("img");
+      image.src = imageSrc;
+      image.onerror = () => {
+        image.src = fallbackImageSrc;
+      };
+
+      const name = document.createElement("div");
+      name.className = "name";
+      name.textContent = sibling;
+
+      siblingContainer.appendChild(image);
+      siblingContainer.appendChild(name);
+
+      container.appendChild(siblingContainer);
+    });
+
+    return container;
+  };
+
+  const createChildrenContainer = (children) => {
+    const container = document.createElement("div");
+    container.className = "container children";
+
+    children.forEach((child) => {
+      const childContainer = document.createElement("div");
+      childContainer.className = "container child";
+
+      const imageSrc = `images/${child.replace(/\s+/g, "").toLowerCase()}.png`;
+      const fallbackImageSrc = "images/male_avatar.png";
+
+      const image = document.createElement("img");
+      image.src = imageSrc;
+      image.onerror = () => {
+        image.src = fallbackImageSrc;
+      };
+
+      const name = document.createElement("div");
+      name.className = "name";
+      name.textContent = child;
+
+      childContainer.appendChild(image);
+      childContainer.appendChild(name);
+
+      container.appendChild(childContainer);
+    });
 
     return container;
   };
